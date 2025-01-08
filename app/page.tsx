51e648df4dd02be83a5838dd0a4d193c5c7b4d77
Alexandra Blomberg <blombergalexandras@gmail.com>
2025-01-08 16:02:39 +0100

    animate header

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Header from '@/components/header'
+import { Header } from '@/components/header'
 import YelloMap from '@/components/yello-map'
 
 export default function Home() {
   return (
-    <>
+    <section className="relative">
       <Header />
       <div className="border-4 rounded-lg border-yellow-500 m-2 h-[400px]">
         <YelloMap />
       </div>
-    </>
+    </section>
   )
 }
