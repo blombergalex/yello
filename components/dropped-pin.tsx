c1e8c1b4127545c2d0026af2ae8c7561525fa0cc
Alexandra Blomberg <blombergalexandras@gmail.com>
2025-01-10 13:13:24 +0100

    improve copy coord experience

--- components/dropped-pin.tsx
+++ components/dropped-pin.tsx
@@ -19,6 +19,7 @@ export const DroppedPin = ({
   const [open, setOpen] = useState<boolean>(false)
   const [showCoords, setShowCoords] = useState<boolean>(false)
   const [routeBtn, setRouteBtn] = useState<boolean>(true)
+  const [copied, setCopied] = useState<boolean>(false)
 
   const positionString = `${skydiverPosition.lat}, ${skydiverPosition.lng}`
 
@@ -44,7 +45,10 @@ export const DroppedPin = ({
     navigator.clipboard
       .writeText(positionString)
       .then(() => {
-        alert('Copied to clipboard!')
+        setCopied(true)
+        setTimeout(() => {
+          setCopied(false)
+        }, 3000)
       })
       .catch((err) => {
         console.error('Failed to copy text: ', err)
@@ -70,41 +74,78 @@ export const DroppedPin = ({
           <h2 className="font-bold">Jane Doe</h2>
           <p>Info about jumper...</p>
           {routeBtn ? (
-            <Button onPress={() => controlRouteBtn()} size="sm" radius="sm" className='bg-yellow-400'>
+            <Button
+              onPress={() => controlRouteBtn()}
+              size="sm"
+              radius="sm"
+              className="bg-yellow-400"
+            >
               Show route
             </Button>
           ) : (
-            <Button onPress={() => controlRouteBtn()} size="sm" radius="sm" className='bg-yellow-400'>
+            <Button
+              onPress={() => controlRouteBtn()}
+              size="sm"
+              radius="sm"
+              className="bg-yellow-400"
+            >
               Hide route
             </Button>
           )}
           {showCoords ? (
             <div className="space-y-1">
-              <Button onPress={() => toggleCoordinates()} size="sm" radius="sm" className='bg-yellow-500'>
+              <Button
+                onPress={() => toggleCoordinates()}
+                size="sm"
+                radius="sm"
+                className="bg-yellow-500"
+              >
                 Hide coordinates
               </Button>
               <div className="flex text-wrap items-center">
-                <h3 className="text-xs">{positionString}</h3>
+                <h3 className="text-xs w-min">{positionString}</h3>
                 <button className="p-2" onClick={() => handleCopy()}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
-                    />
-                  </svg>
+                  {copied ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
           ) : (
-            <Button onPress={() => toggleCoordinates()} size="sm" radius="sm" className='bg-yellow-500'>
+            <Button
+              onPress={() => toggleCoordinates()}
+              size="sm"
+              radius="sm"
+              className="bg-yellow-500"
+            >
               Show coordinates
             </Button>
           )}
