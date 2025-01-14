d5f507d47a91015a01c80b73a6402a420dadb2e1
Alexandra Blomberg <blombergalexandras@gmail.com>
2025-01-14 09:51:07 +0100

    separate directions component from map

---
+++ components/directions.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react"
+import { useMap, useMapsLibrary } from '@vis.gl/react-google-maps'
+
+export const Directions = () => {
+    const map = useMap()
+    const routesLibrary = useMapsLibrary('routes')
+    const [directionsService, setDirectionsService] =
+      useState<google.maps.DirectionsService>()
+    const [directionsRenderer, setDirectionsRenderer] =
+      useState<google.maps.DirectionsRenderer>()
+    const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([])
+    const [userCoords, setUserCoords] = useState<string | null>(null)
+
+    console.log(routes)
+
+    useEffect(() => {
+      const getLocation = () => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position: GeolocationPosition) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              }
+
+              const userCoords = `${pos.lat},${pos.lng}`
+              setUserCoords(userCoords)
+            }
+          )
+        } else {
+          console.error('Geolocation is not supported by browser')
+        }
+      }
+
+      getLocation()
+    }, [])
+
+    useEffect(() => {
+      if (!routesLibrary || !map) return
+
+      const renderer = new routesLibrary.DirectionsRenderer({ map })
+      setDirectionsService(new routesLibrary.DirectionsService())
+      setDirectionsRenderer(renderer)
+
+      return () => {
+        if (renderer) {
+          renderer.setMap(null)
+        }
+      }
+    }, [routesLibrary, map])
+
+    useEffect(() => {
+      if (!directionsService || !directionsRenderer || !userCoords) return
+
+      directionsService
+        .route({
+          origin: `${userCoords}`,
+          destination: '60.286041259765625, 17.425235748291016',
+          travelMode: google.maps.TravelMode.DRIVING,
+          provideRouteAlternatives: false,
+        })
+        .then((response) => {
+          directionsRenderer.setDirections(response)
+          setRoutes(response.routes)
+        })
+    }, [directionsService, directionsRenderer, userCoords])
+
+    return null
+  }
--- components/yello-map.tsx
+++ components/yello-map.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { Map, useMap, useMapsLibrary } from '@vis.gl/react-google-maps'
+import { Map } from '@vis.gl/react-google-maps'
 
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { DroppedPin } from './dropped-pin'
 import { getPins } from '@/utils/supabase/queries'
+import { Directions } from './directions'
 
 const position = {
   lat: 60.286041259765625,
@@ -13,92 +14,12 @@ const position = {
 
 const YelloMap = () => {
   const [showDirections, setShowDirections] = useState<boolean>(false)
+  // const [error, setError] = useState<string | null>(null)
+  // const [pinData, setPinData] = useState<string[] | null>(null)
 
   const toggleShowDirections = () => {
     setShowDirections(!showDirections)
   }
-
-  const Directions = () => {
-    const map = useMap()
-    const routesLibrary = useMapsLibrary('routes')
-    const [directionsService, setDirectionsService] =
-      useState<google.maps.DirectionsService>()
-    const [directionsRenderer, setDirectionsRenderer] =
-      useState<google.maps.DirectionsRenderer>()
-    const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([])
-    const [userCoords, setUserCoords] = useState<string | null>(null)
-
-    console.log(routes)
-
-    useEffect(() => {
-      const getLocation = () => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position: GeolocationPosition) => {
-              const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-              }
-
-              const userCoords = `${pos.lat},${pos.lng}`
-              setUserCoords(userCoords)
-            }
-          )
-        } else {
-          console.error('Geolocation is not supported by browser')
-        }
-      }
-
-      getLocation()
-    }, [])
-
-    useEffect(() => {
-      if (!routesLibrary || !map) return
-
-      const renderer = new routesLibrary.DirectionsRenderer({ map })
-      setDirectionsService(new routesLibrary.DirectionsService())
-      setDirectionsRenderer(renderer)
-
-      return () => {
-        if (renderer) {
-          renderer.setMap(null)
-        }
-      }
-    }, [routesLibrary, map])
-
-    useEffect(() => {
-      if (!directionsService || !directionsRenderer || !userCoords) return
-
-      directionsService
-        .route({
-          origin: `${userCoords}`,
-          destination: '60.286041259765625, 17.425235748291016',
-          travelMode: google.maps.TravelMode.DRIVING,
-          provideRouteAlternatives: false,
-        })
-        .then((response) => {
-          directionsRenderer.setDirections(response)
-          setRoutes(response.routes)
-        })
-    }, [directionsService, directionsRenderer, userCoords])
-
-    return null
-  }
-
-
-  useEffect(() => {
-    const fetchPins = async () => {
-      const { data, error } = await getPins()
-      if (error) {
-        console.error(error)
-      } else {
-        console.log({ data })
-      }
-    }
-
-    fetchPins()
-  })
-
 
   return (
     <>
@@ -111,9 +32,26 @@ const YelloMap = () => {
           {showDirections && <Directions />}
         </Map>
       </div>
+      {/* {error || pinData!.length === 0 ? (
+        <p>No dropped pins yet!</p>
+      ) : (
+        pinData!.map(({ id, created_at, coordinates, description, users, injured}) => (
+          <DroppedPin color="yellow" getDirections={toggleShowDirections} 
+            key={id}
+            name={users?.name || "unknown"}
+            time={created_at}
+            description={description}
+            injured={injured}
+            coordinates={coordinates}
+          />
+        ) )}
+      )  */}
       <DroppedPin color="yellow" getDirections={toggleShowDirections} />
     </>
   )
 }
 
 export default YelloMap
+
+
+
