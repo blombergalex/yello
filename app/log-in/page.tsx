4bb244278171eae9be017138d39c116abcca214d
Alexandra Blomberg <blombergalexandras@gmail.com>
2025-01-22 10:15:23 +0100

    add home link to login

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -11,9 +11,15 @@ export default function LogInPage() {
         <h1 className='text-2xl font-bold text-yellow-400'>Welcome</h1>
         <LogInForm />
       </div>
-      <Link href='/sign-up' className='text-yellow-500 text-small hover:underline underline-offset-2'>
-        Don&quot;t have an account? Sign up
-      </Link>
+      <div className='flex  flex-col items-center'>
+        <Link href='/sign-up' className='text-yellow-500 text-small hover:underline underline-offset-2'>
+          Don&quot;t have an account? Sign up
+        </Link>
+        <p className='text-gray-500'>or</p>
+        <Link href='/' className='text-yellow-500 text-small hover:underline underline-offset-2'>
+          View map without account
+        </Link>
+      </div>
     </main>
   )
 }
