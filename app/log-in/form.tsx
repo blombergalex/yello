7d52de1a79b8f235b516d35e48e4efe69525a42d
Alexandra Blomberg <blombergalexandras@gmail.com>
2025-01-23 12:37:00 +0100

    move suspense to page

--- app/log-in/form.tsx
+++ app/log-in/form.tsx
@@ -2,7 +2,7 @@
 
 import { Button, Input } from '@nextui-org/react'
 import { toast } from 'sonner'
-import { useEffect, Suspense } from 'react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useMutation } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
@@ -14,7 +14,6 @@ import { buttonClasses, errorClasses, inputClasses } from '@/utils/classes'
 import { handleServerError } from '@/utils/action-utils'
 import { logIn } from '@/actions/log-in'
 import { handleRedirect } from '@/utils/handle-redirect'
-import Spinner from '@/components/spinner'
 
 export const LogInForm = () => {
   const searchParams = useSearchParams()
@@ -46,47 +45,45 @@ export const LogInForm = () => {
   })
 
   return (
-    <Suspense fallback={<Spinner />}>
-      <form
-        onSubmit={handleSubmit((values) => mutate(values))}
-        className="flex w-full flex-col max-w-md gap-4"
-      >
-        <div className="flex flex-col gap-4 items-center mx-4">
-          <div className="w-2/3">
-            <Input
-              className={inputClasses}
-              {...register('email')}
-              type="text"
-              label="Email"
-              name="email"
-              required
-            />
-            {errors.email && (
-              <span className={errorClasses}>{errors.email.message}</span>
-            )}
-          </div>
-          <div className="w-2/3">
-            <Input
-              className={inputClasses}
-              {...register('password')}
-              type="password"
-              label="Password"
-              required
-            />
-            {errors.password && (
-              <span className={errorClasses}>{errors.password.message}</span>
-            )}
-          </div>
-          <Button
-            className={buttonClasses}
-            type="submit"
-            size="sm"
-            disabled={isPending}
-          >
-            {isPending ? 'Logging in' : 'Log in'}
-          </Button>
+    <form
+      onSubmit={handleSubmit((values) => mutate(values))}
+      className="flex w-full flex-col max-w-md gap-4"
+    >
+      <div className="flex flex-col gap-4 items-center mx-4">
+        <div className="w-2/3">
+          <Input
+            className={inputClasses}
+            {...register('email')}
+            type="text"
+            label="Email"
+            name="email"
+            required
+          />
+          {errors.email && (
+            <span className={errorClasses}>{errors.email.message}</span>
+          )}
         </div>
-      </form>
-    </Suspense>
+        <div className="w-2/3">
+          <Input
+            className={inputClasses}
+            {...register('password')}
+            type="password"
+            label="Password"
+            required
+          />
+          {errors.password && (
+            <span className={errorClasses}>{errors.password.message}</span>
+          )}
+        </div>
+        <Button
+          className={buttonClasses}
+          type="submit"
+          size="sm"
+          disabled={isPending}
+        >
+          {isPending ? 'Logging in' : 'Log in'}
+        </Button>
+      </div>
+    </form>
   )
 }
--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,27 +1,35 @@
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 import { LogInForm } from './form'
 import { Logo } from '@/components/logo'
+import Spinner from '@/components/spinner'
 
 export default function LogInPage() {
-
-
   return (
-    <main className='main flex h-screen flex-col items-center justify-center gap-6'>
-      <Logo />
-      <div className='flex w-full flex-col items-center gap-12'>
-        <h1 className='text-2xl font-bold text-yellow-400'>Welcome</h1>
-        <LogInForm />
-      </div>
-      <div className='flex  flex-col items-center'>
-        <Link href='/sign-up' className='text-yellow-500 text-small hover:underline underline-offset-2'>
-          Don&quot;t have an account? Sign up
-        </Link>
-        <p className='text-gray-400 text-small'>or</p>
-        <Link href='/' className='text-yellow-500 text-small hover:underline underline-offset-2'>
-          View without account
-        </Link>
-      </div>
-    </main>
+    <Suspense fallback={<Spinner />}>
+      <main className="main flex h-screen flex-col items-center justify-center gap-6">
+        <Logo />
+        <div className="flex w-full flex-col items-center gap-12">
+          <h1 className="text-2xl font-bold text-yellow-400">Welcome</h1>
+          <LogInForm />
+        </div>
+        <div className="flex  flex-col items-center">
+          <Link
+            href="/sign-up"
+            className="text-yellow-500 text-small hover:underline underline-offset-2"
+          >
+            Don&quot;t have an account? Sign up
+          </Link>
+          <p className="text-gray-400 text-small">or</p>
+          <Link
+            href="/"
+            className="text-yellow-500 text-small hover:underline underline-offset-2"
+          >
+            View without account
+          </Link>
+        </div>
+      </main>
+    </Suspense>
   )
 }
